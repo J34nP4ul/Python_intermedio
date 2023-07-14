ab75e3eb905d8cf46dd3fb6448f60484a4d446c2
jean <jean@pop-os.localdomain>
2023-07-13 22:20:22 -0500

    Laboratorio 2

---
+++ laboratorio1/trig.py
@@ -0,0 +1,17 @@
+import math
+
+#creando el atributo pi
+pi = math.pi
+
+#creando una clase trig
+class trig:
+    
+    def __init__ (self, angulo):
+        self.angulo = angulo
+        self.seno = math.sin(math.radians(angulo))
+        self.coseno = math.cos(math.radians(angulo))
+        self.tangente = math.tan(math.radians(angulo))
+        
+resultado = trig (pi)
+
+print ("seno: ", resultado.seno)
---
+++ main.py
@@ -0,0 +1,30 @@
+import api
+import requests
+import ids
+import time
+import cProfile
+import pstats
+
+from api import getOneUser
+from ids import ids as lista
+
+def progLineal():
+    tiempoInicial = time.time()
+    for ids in lista:
+        nuevaLista = getOneUser(ids)
+        print(f"Nombre del usuario con ID {ids}: {nuevaLista}")
+
+    tiempoFinal = time.time()
+    tiempoTotal = tiempoFinal - tiempoInicial
+
+    #print(f"Tiempo de duración: {tiempoTotal} segundos")
+    
+def main ():
+    progLineal()
+
+if __name__ == '__main__':
+    cProfile.run('main()', filename='profiling_results')
+
+stats = pstats.Stats('profiling_results')
+stats.sort_stats('cumulative')
+stats.print_stats()
