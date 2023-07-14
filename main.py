0902088021e255f99d5ee00e955926a73a4358f5
Jean Paul <marsys.nathan@gmail.com>
2023-07-13 22:47:31 -0500

    laboratorio1

--- main.py
+++ main.py
@@ -17,10 +17,22 @@ def progLineal():
     tiempoFinal = time.time()
     tiempoTotal = tiempoFinal - tiempoInicial
 
-    #print(f"Tiempo de duración: {tiempoTotal} segundos")
+    print(f"Tiempo de duración: {tiempoTotal} segundos")
     
+def progConcurrencia():
+    tiempoInicial = time.time()
+    for ids in lista:
+        nuevaLista = getOneUser(ids)
+        print(f"Nombre del usuario con ID {ids}: {nuevaLista}")
+
+    tiempoFinal = time.time()
+    tiempoTotal = tiempoFinal - tiempoInicial
+
+    print(f"Tiempo de duración: {tiempoTotal} segundos")
+#   
 def main ():
-    progLineal()
+    #progLineal()
+    progConcurrencia()
 
 if __name__ == '__main__':
     cProfile.run('main()', filename='profiling_results')
